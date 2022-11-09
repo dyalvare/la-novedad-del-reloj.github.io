acfe69deb6ea3d84019219d2d0f25e27239c5d22
dyalvare <117835172+dyalvare@users.noreply.github.com>
2022-11-09 09:01:15 -0500

    Initial commit

---
+++ js/products.js
@@ -0,0 +1,50 @@
+const productos = [
+    {   
+        id: 1,
+        nombre: "Topos de oro", 
+        precio: 50000,
+        img: 
+        "./imagenes/topos_de_oro.jpg",
+        cantidad: 1,
+    },
+    {   
+        id: 2,
+        nombre: "Cadena de plata", 
+        precio: 50000,
+        img: 
+        "./imagenes/cadenas_plata.png",
+        cantidad: 1,
+    },
+    {   
+        id: 3,
+        nombre: "Cadena para hombre", 
+        precio: 50000,
+        img: 
+        "./imagenes/cadenas_hombre.png",
+        cantidad: 1,
+    },
+    {   
+        id: 4,
+        nombre: "Cadena para dama", 
+        precio: 50000,
+        img: 
+        "./imagenes/cadenas_dama.png",
+        cantidad: 1,
+    },
+    {   
+        id: 5,
+        nombre: "Anillo de oro dama", 
+        precio: 50000,
+        img: 
+        "./imagenes/anillos_dama.jpg",
+        cantidad: 1,
+    },
+    {   
+        id: 6,
+        nombre: "Dijes de oro", 
+        precio: 50000,
+        img: 
+        "./imagenes/dijes_oro.jpg",
+        cantidad: 1,
+    },
+];
